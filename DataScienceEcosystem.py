5b77b1d140c514641bf9dac63830bf6478a770b0
Ali Abdo Mafiol <57445362+turquiz@users.noreply.github.com>
2023-04-03 14:07:18 -0500

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives:**
+# Data Science Tools
+# - PostgreSQL
+# - Apache AirFlow
+# - Seldon
+# - TensorFlow
+# - Kibana
+
+# ## Author 
+# Ali Abdo Mafiol
+
+# Some of the popular languages that Data Scientists use are:
+# - Python
+# - R
+# - SQL
+# 
+
+# Some of the commonly used libraries used by Data Scientists include:
+# - Scikit-learn
+# - NumPy 
+# - SciPy
+
+# Data Science Tools
+# 
+# - PostgreSQL
+# - Apache AirFlow
+# - Seldon
+# - TensorFlow
+# - Kibana
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+# 
+#     -(3 + 4)
+#     -(7 - 3) 
+#     -(3*4)+5
+
+# In[7]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[9]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+200/60
+
